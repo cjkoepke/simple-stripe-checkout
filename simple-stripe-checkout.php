7f190ae235e36fa9c94aebad1c8950c1d3a2bb4b
Calvin Koepke <hello@calvinkoepke.com>
2019-06-17 08:34:23 -0600

    WIP: add php linting

--- simple-stripe-checkout.php
+++ simple-stripe-checkout.php
@@ -4,20 +4,21 @@
  * Users are directed off-site to complete purchases, and upon success or cancel redirected to pre-set pages on your site.
  * This plugin stores NO data from your transactions, other than caching Stripe products and your public API key.
  *
- * @author Freeshifter LLC
- * @url https://www.freeshifter.com
- * @since 1.0.0
+ * @author  Freeshifter LLC <contact@freeshifter.com>
+ * @url     https://www.freeshifter.com
+ * @license GPL-3.0+ https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @since   1.0.0
+ * @package Freeshifter\Init
  */
 
 namespace Freeshifter;
 
-/**
+/*
  * Load the plugin.
- *
- * @since 1.0.0
  */
-add_action( 'plugins_loaded', function() {
-
-	require_once( __DIR__ . '/classes/SSC_Admin.php' );
-
-});
+add_action(
+	'plugins_loaded',
+	function() {
+		require_once __DIR__ . '/classes/SSC_Admin.php';
+	}
+);
